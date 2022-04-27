b2fe16a225cacdcef9921c47e275e19cb15e7a69
Karol Mileszko <karol.miles@gmail.com>
2022-04-27 20:07:08 +0000

    Complete sql-crud.py tutorial

--- sql-crud.py
+++ sql-crud.py
@@ -95,10 +95,53 @@ karol_mileszko = Programmer(
 # session.add(margaret_hamilton)
 # session.add(bill_gates)
 # session.add(tim_berners_lee)
-session.add(karol_mileszko)
+# session.add(karol_mileszko)
+
+
+# updating a single record
+# programmer = session.query(Programmer).filter_by(id=7).first()
+# programmer.famous_for = "World President"
+
+
+# updating multiple records
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("Gender not defined")
+#     session.commit()
+
+
+# deleting a single record
+# fname = input("Enter a first name: ")
+# lname = input("Enter a last name: ")
+# programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+# defensive programming
+# if programmer is not None:
+#     print("Programmer found: ", programmer.first_name + " " + programmer.last_name)
+#     confirmation = input("Are you sure you want to delete this record? (y/n) ")
+#     if confirmation.lower() == "y":
+#         session.delete(programmer)
+#         session.commit()
+#         print("Programmer has been deleted")
+#     else:
+#         print("Programmer not deleted")
+# else:
+#     print("No records found")
+
 
 # commit our session to the database
-session.commit()
+# session.commit()
+
+# delete multiple records - DO NOT USE
+# programmers = session.query(Programmer)
+# for programmer in programmers:
+#     session.delete(programmer)
+#     session.commit()
+# DO NOT USE - END
 
 # query the database to find all Programmers
 programmers = session.query(Programmer)
